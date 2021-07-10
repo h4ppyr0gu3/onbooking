585304da8d714aa237f365c5c37e83b14c8fd834
h4ppyr0gu3 <rogersdpdr@gmail.com>
2021-07-10 18:47:46 +0200

    fixed padding and burger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,14 +16,18 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-
+  # config.assets.compile = true
   config.active_storage.service = :local
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
-  config.assets.debug = true
+  config.assets.debug = false
+  config.assets.unknown_asset_fallback = true
+  config.assets.export_concurrent = false
+  config.assets.check_precompiled_asset = false
   config.assets.quiet = true
+  # config.serve_static_files = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
