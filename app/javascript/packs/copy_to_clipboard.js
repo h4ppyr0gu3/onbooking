94b77e6a5a627ff48c07c376b637cc39805d8b64
h4ppyr0gu3 <rogersdpdr@gmail.com>
2021-07-10 17:50:46 +0200

    ui fixed

--- app/javascript/packs/copy_to_clipboard.js
+++ app/javascript/packs/copy_to_clipboard.js
@@ -1,13 +1,12 @@
 var copyButtons = document.getElementsByClassName("copy");
-console.log(copyButtons)
-for (let i = 0; i < copyButtons.length; i++ ) {
-	copyButtons[i].addEventListener("click", (e) => {
-		console.log(e)
-		console.log(e.target)
-		console.log(e.target.name)
-		navigator.clipboard.writeText(e.target.name).then(function() {
-		}, function() {
-		  alert("failed")
-		});
-	})
-}
+
+document.addEventListener('turbolinks:load', (event) => {
+	for (let i = 0; i < copyButtons.length; i++ ) {
+		copyButtons[i].addEventListener("click", (e) => {
+			navigator.clipboard.writeText(window.location.host + e.target.name).then(function() {
+			}, function() {
+			  alert("failed")
+			});
+		})
+	}
+});
